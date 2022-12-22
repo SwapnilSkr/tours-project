b6228c06d3837c48688ee44b28d4c966e1ce3b1d
Swapnil Sarkar <swapnilmkab@gmail.com>
2022-12-23 04:50:17 +0530

    Created a controller file to store user route handlers

---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    //500 is called internal error.
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.PostUser = (req, res) => {
+  res.status(500).json({
+    //500 is called internal error.
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.getUserbyId = (req, res) => {
+  res.status(500).json({
+    //500 is called internal error.
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.UpdateUser = (req, res) => {
+  res.status(500).json({
+    //500 is called internal error.
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.DeleteUser = (req, res) => {
+  res.status(500).json({
+    //500 is called internal error.
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
