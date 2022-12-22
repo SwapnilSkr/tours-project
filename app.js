0610fa2f2dbe9dbce1a4a249f8a76d5e60b596c7
Swapnil Sarkar <swapnilmkab@gmail.com>
2022-12-23 03:24:09 +0530

    Refactoring routes in separate files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes'); //fetching the tourRoutes package that we created.
+const userRouter = require('./routes/userRoutes'); //fetching the userRoutes package that we created.
 const app = express();
 
 //1) MIDDLEWARE
@@ -30,190 +31,13 @@ app.use((req, res, next) => {
   next();
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`) //tours stores the array of objects from tours-simple...
-);
-
-//2) ROUTER FUNCTIONS
-
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-};
-
-const getTourbyId = (req, res) => {
-  //? makes the parameter optional.
-  //This :var is used to specify the variable
-  console.log(
-    req.params
-  ); /*req.params is where the variables defined in the routes are stored. It is an object which automatically 
-  assigns the value to our variable that we defined in the route.*/
-
-  const id = req.params.id * 1; // this is used to turn the id key in the params object from a string to an integer
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  const tour = tours.find(
-    (ele) => ele.id === id
-  ); /* This find method is used to iterate through the tours array until it finds the
-  object with an id equal to the variable passed as the parameter in the route.*/
-
-  /* if (!tour) {
-    return res.status(404).json({         Another way to handle error responses.
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }*/
-
-  res.status(200).json({
-    status: 'success',
-    //result: tours.length,
-    data: {
-      tour,
-    },
-  });
-};
-
-const CreateTour = (req, res) => {
-  /* console.log(
-    req.body
-  ); body is the property that is gonna be available on the request because we used the middleware on line 4. */
-
-  /*res.send(
-    'Done'
-  ); We always need to send back something in order to finish the request/response cycle*/
-
-  const newId =
-    tours[tours.length - 1].id +
-    1; /* This is used to increment the id of the new tour by adding 1 to the last tour's id*/
-
-  const newTour = Object.assign(
-    { id: newId },
-    req.body
-  ); /*Object.assign allows us to create a new object by merging two existing 
-  objects together. Here the first object is the newId and the second one is the body that we created in postman. */
-
-  tours.push(
-    newTour
-  ); /*Pushing the newTour object into the tours array of objects. */
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tours: newTour,
-        },
-      });
-    }
-  );
-};
-
-const UpdateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here....>', //We have not implemented any updates, instead in order to understand we have used placeholder.
-    },
-  });
-};
-
-const DeleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null, // Similarly like the previous update request we haven't implemented the delete javascript, just a demo.
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    //500 is called internal error.
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const PostUser = (req, res) => {
-  res.status(500).json({
-    //500 is called internal error.
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const getUserbyId = (req, res) => {
-  res.status(500).json({
-    //500 is called internal error.
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const UpdateUser = (req, res) => {
-  res.status(500).json({
-    //500 is called internal error.
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const DeleteUser = (req, res) => {
-  res.status(500).json({
-    //500 is called internal error.
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-//3) ROUTES
+//2) ROUTES
 
 /*app.get('/api/v1/tours', getAllTours);
 app.get('/api/v1/tours/:id', getTourbyId);
 app.post('/api/v1/tours', CreateTour);               Refactoring the routes 
 app.patch('/api/v1/tours/:id', UpdateTour);
 app.delete('/api/v1/tours/:id', DeleteTour);*/
-
-const tourRouter =
-  express.Router(); /*In order to bundle the routers in a better manner, we are using separate routers for both 
-resources*/
-const userRouter = express.Router();
-
-tourRouter.route('/').get(getAllTours).post(CreateTour); // ('/') is used in place of ('/api/v1/tours).
-tourRouter
-  .route('/:id')
-  .get(getTourbyId) //Refactoring the routes in a better way by chaining them together.
-  .patch(UpdateTour)
-  .delete(DeleteTour);
-
-userRouter.route('/').get(getAllUsers).post(PostUser);
-userRouter.route('/:id').get(getUserbyId).patch(UpdateUser).delete(DeleteUser);
 
 app.use('/api/v1/tours', tourRouter); //Mounting the routers
 app.use('/api/v1/users', userRouter); //We have to use the router middleware at the end.
@@ -223,7 +47,7 @@ the entire new updated object, and with patch, we only expect the properties tha
 generally use patch  because it is easier to simply update the properties that we want to update instead of returning the entire
 object. So we are going to make our app work for patch and not put.*/
 
-//4) START THE SERVER
+//3) START THE SERVER
 
 const port = 3000;
 app.listen(port, () => {
